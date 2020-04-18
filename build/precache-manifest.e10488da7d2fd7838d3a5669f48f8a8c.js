244a87e1d53c62b8edcd66721117c887bf58b865
Куранов Антон <ya.kuranov@gmail.com>
2020-04-18 03:53:04 +0300

    added build dir to repository

---
+++ build/precache-manifest.e10488da7d2fd7838d3a5669f48f8a8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf42eb91c990f2792743f99a41044c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bed980bfeac7e0c2e26",
+    "url": "/static/css/main.8d2c5377.chunk.css"
+  },
+  {
+    "revision": "f45bc6fff5340701a3c4",
+    "url": "/static/js/2.cd99ffd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cd99ffd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bed980bfeac7e0c2e26",
+    "url": "/static/js/main.06b189dc.chunk.js"
+  },
+  {
+    "revision": "f1c3230568f5e07082ea",
+    "url": "/static/js/runtime-main.0dc6ea7e.js"
+  }
+]);
