c9bb80da7744bb6d916ac94a546700f1da568af6
Куранов Антон <ya.kuranov@gmail.com>
2020-04-20 10:21:28 +0300

    delete 'build' folder

--- src/components/Minefield/Minefield.tsx
+++ src/components/Minefield/Minefield.tsx
@@ -39,17 +39,15 @@ const Minefield: FunctionComponent<IMinefieldProps> = ({
 
   useEffect(() => {     
     const plantedMines = plantMines(width, height, mines);
-    const tilesGrid = generateTilesGrid(width, height, (x, y) => {
-      return {
+    const tilesGrid = generateTilesGrid(width, height, (x, y) => ({
         x, 
         y, 
         minesAround: findMinesAround(plantedMines, x, y),
         isMine: isMine(plantedMines, x, y),
         isFlagged: false,
         isOpen: false
-      };
-    });
-
+      })
+    );
     setTilesGrid(tilesGrid);
   }, [width, height, mines, startedAt]);
 
@@ -97,21 +95,21 @@ const Minefield: FunctionComponent<IMinefieldProps> = ({
 
     while(q.size > 0) {
       const coords = q.dequeue();
-      if (coords) {
+      if (coords) { 
         const [x, y] = coords;
         for(let i = -1; i <= 1; i++) {
           for(let j = -1; j <= 1; j++) {
-              if (j === 0 && i === 0) {
-                  continue;
+            if (j === 0 && i === 0) {
+                continue;
+            }
+            let checkX = x + i, checkY = y + j;
+            if (tilesGrid && tilesGrid[checkY] && tilesGrid[checkY][checkX]) {
+              let cell = tilesGrid[checkY][checkX];
+              if (!cell.isOpen && cell.minesAround === 0) {
+                cell.isOpen = true;
+                q.enqueue([checkX, checkY]);
               }
-              let checkX = x + i, checkY = y + j;
-              if (tilesGrid && tilesGrid[checkY] && tilesGrid[checkY][checkX]) {
-                let cell = tilesGrid[checkY][checkX];
-                if (!cell.isOpen && cell.minesAround === 0) {
-                  cell.isOpen = true;
-                  q.enqueue([checkX, checkY]);
-                }
-              }
+            }
           }
         }
       }
@@ -120,7 +118,6 @@ const Minefield: FunctionComponent<IMinefieldProps> = ({
     return tilesGrid;
   };
 
-
   const isMine = (mines: IMinesMap, x: number, y: number): boolean => !!mines[x + '-' + y];
 
   const findMinesAround = (minesMap: IMinesMap, x: number, y: number): number => {
@@ -128,10 +125,10 @@ const Minefield: FunctionComponent<IMinefieldProps> = ({
     for(let i = -1; i <= 1; i++) {
         for(let j = -1; j <= 1; j++) {
             if (j === 0 && i === 0) {
-                continue;
+              continue;
             }
             if (isMine(minesMap, (x + i), (y + j))) {
-                bombsAround++;
+              bombsAround++;
             }
         }
     }
